c4703e11091e35d92a715a68aa3106f725ef1491
lawrend <douglas_lawrence@mac.com>
2019-03-16 15:06:54 -0700

    updated ruby to 2.6.0 and rails - resolved rails console issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Collection.create(title:"Rembrants", description:"Lots of that dude!")
+Collection.create(title:"Dope pics of Jesus", description:"lil guy made it into lots of pics!")
