d19764464b135688b6c57149fee08ffca9aeb01c
lawrend <douglas_lawrence@mac.com>
2019-03-20 14:01:59 -0700

    generated user and modified route; migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
     namespace :v1 do
       resources :works
       resources :collections
     end
   end
+  root "controllers#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
